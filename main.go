d44d3816a4af38802f158b72e300f18ddd658053
Reno D Hermawan <reno.d.hermawan@gmail.com>
2021-03-30 10:37:59 +0700

    add leap year method

--- main.go
+++ main.go
@@ -1,19 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var input Year
-	fmt.Print("Input: ")
+	fmt.Print("Input first year: ")
 	fmt.Scan(&input.firstInput)
+	fmt.Print("Input last year: ")
+	fmt.Scan(&input.lastInput)
 
-	fmt.Println(input.firstInput)
+	leapYear, err := input.leapYearDeterminer()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Leap's year: ")
+	for _, value := range leapYear {
+		fmt.Println(value)
+	}
 }
 
 type Year struct {
 	firstInput string
+	lastInput  string
 }
 
-func (y Year) leapYearDeterminer() (string, error) {
-	return y.firstInput, nil
+func (y Year) leapYearDeterminer() ([]int, error) {
+	firstInput, err1 := strconv.Atoi(y.firstInput)
+	lastInput, err2 := strconv.Atoi(y.lastInput)
+	var output []int
+
+	//errror handler
+	if err1 != nil || err2 != nil {
+		return nil, fmt.Errorf("Input isn't number")
+	}
+
+	//swap value to make the lower value only for first variable
+	if lastInput < firstInput {
+		firstInput, lastInput = lastInput, firstInput
+	}
+
+	//mod by 4 equal to 0
+	for i := firstInput; i <= lastInput; i++ {
+		if i%4 == 0 {
+			output = append(output, i)
+		}
+	}
+
+	//for every new century that mod by 100 and 400 equal to 0
+	for j := 0; j < len(output); j++ {
+		if output[j]%100 == 0 && output[j]%400 != 0 {
+			output = append(output[:j], output[j+1:]...)
+		}
+	}
+
+	return output, nil
 }
